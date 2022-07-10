1c754b72dd151417710e9d29db43692806a9e952
MarouaneBouf <104838272+MarouaneBouf@users.noreply.github.com>
2022-07-10 23:12:14 +0100

    Create SumLimits.py

---
+++ SumLimits.py
@@ -0,0 +1,15 @@
+#Example:--------------------------
+#>>> md
+#>>> md5fg27dm5p
+#Between the first letter m and the last letter d, we have the values 5 and 27.
+#Thus, we obtain: 5 + 27 = 32 -------------------
+
+import re
+n = input()
+s = input()
+
+try:
+    word = s[s.index(n[0]):s.rindex(n[len(n)-1])+1]
+    print(sum(list(map(int,re.findall("\d+",word)))))
+except ValueError:
+    print(0)
