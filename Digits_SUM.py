0e1f309506aa652fa0c0c409c96188abccc8ec98
KingZ47 <96877773+KingZ47@users.noreply.github.com>
2022-12-16 22:59:10 +0100

    Update Digits_SUM.py

--- Digits_SUM.py
+++ Digits_SUM.py
@@ -1,9 +1,12 @@
 #sum the digits of a given number untill its length is equal to 1:
 --->#Implementation
-n=int(input())
-if n==0:
-    print(0)
-elif n%9==0:
-    print(9)
-else:
-    print(n%9)
+def main():
+    n=int(input())
+    if n==0:
+        print(0)
+    elif n%9==0:
+        print(9)
+    else:
+        print(n%9)
+if __name__ == '__main__':
+    main()
